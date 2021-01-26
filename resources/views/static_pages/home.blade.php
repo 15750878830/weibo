8f1d927ec6991b0080b05fd090a79dae1154a296
vagrant <vagrant@homestead>
2021-01-26 12:27:37 +0000

    基础页面

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        {{-- <title>Weibo App</title> --}}
+    </head>
+    <body>
+        @extends('layouts.default')
+        @section('content')
+        <h1>主页</h1>
+        @stop
+    </body>
+</html>
